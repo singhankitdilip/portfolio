39b89f395f028a8104ab8571b3c8ef2baddd780d
Ankit Singh <110975418+singhankitdilip@users.noreply.github.com>
2023-11-30 17:34:37 +0530

    Update Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -32,9 +32,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
-          from_name: form.name,
+          name: form.name,
           to_name: "Ankit Singh",
-          from_email: form.email,
+          email: form.email,
           to_email: "singhankitdilip@gmail.com",
           message: form.message,
         },
